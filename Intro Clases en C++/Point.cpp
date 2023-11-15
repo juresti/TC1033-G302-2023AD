1622e3650ff8372cd6852cfd90d02d7b584aea2f
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2023-11-15 14:13:11 -0600

    Codigo de prueba de la clase
    Del ejemplo visto la clase pasada

--- Intro Clases en C++/Point.cpp
+++ Intro Clases en C++/Point.cpp
@@ -1,4 +1,7 @@
 #include <cmath>
+#include <iostream>
+
+using namespace std;
 
 class Point{
 private:
@@ -35,3 +38,19 @@ public:
         return sqrt(pow(p.getX()-getX(),2) + pow(p.getY()-getY(),2));
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    Point p1 {}; //default
+    cout << "Valor de x = " << p1.getX() << endl;
+    p1.setX(7);
+    cout << "Valor de x = " << p1.getX() << endl;
+    
+    Point p2 {13,21}; //2 paramatros
+    cout << "Valor de y = " << p2.getY() << endl;
+    p2.setY(42);
+    cout << "Valor de y = " << p2.getY() << endl;
+
+    cout << "La distancia entre p1 y p2 es " << p1.distance(p2) << endl;
+    return 0;
+}
